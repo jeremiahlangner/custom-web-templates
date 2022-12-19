d907705c6bc253829fd5e15e8b19f3aa554b250d
Home <home@COCOOPs-iMac-3.local>
2022-12-19 16:48:46 -0700

    fix observer

---
+++ src/workers/eval.worker.ts
@@ -0,0 +1,23 @@
+let _data = {};
+
+self.onMessage((e: MessageEvent<string>) => {
+  console.log(e);
+  const events = {
+    setData: (data) => {
+      if (_data == data) return;
+      _data = data;
+    },
+    eval: (statement: string) => {
+      /*
+       * Notes: 
+       *
+       * Need to be able to evaluate in local context, or full data context.
+       * Local data should just reference a key in the full data context to
+       * prevent reseting it.
+       */
+      
+    }
+  };
+});
+
+
--- src/workers/safeEval.ts
+++
@@ -1,9 +0,0 @@
-
-self.onMessage(e: MessageEvent<string>) {
-  console.log(e);
-  if (e.setData) {
-    data = e.setData;
-  } elseif
-}
-
-
