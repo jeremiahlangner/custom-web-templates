aaad0490b62f93e12ea37871ef27735c7ee651cf
Home <home@COCOOPs-iMac-3.lan>
2022-12-21 12:41:57 -0700

    add component definitions to assets

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,34 @@
-class App {
- constructor() {
-  
- }
-}
+import { Observed } from './services/observed.service';
 
+class App {
+  serviceWorker: Worker;
+  changeList = [];
+
+  constructor() {
+    window.vcdc = {
+      services: {},
+      app: {}
+    };
+
+    const data = new Observed({}, () => void 0, this.changes);
+    this.registerServices(); 
+  }
+
+  changes() {
+    for(const fn of changeList) {
+      fn();
+    }
+  }
+
+  registerWorkers() {
+    this.serviceWorker = new ServiceWorker('./workers/service.worker.js'); 
+  }
+
+  registerServices() {
+    window.vcdc.services = {
+      storage: new StorageService(),
+    };
+  }
+}
 const app = new App();
 
