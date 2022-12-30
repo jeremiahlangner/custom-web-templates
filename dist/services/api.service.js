3653ceff940ba0e8443764f396ff5861d7630d83
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2022-12-30 15:11:48 -0700

    update dist

--- dist/services/api.service.js
+++ dist/services/api.service.js
@@ -1,1 +1,1 @@
-(()=>{var n=class{constructor(){}async request(e){let s=new Request(e.url,e);return fetch(s).then(t=>e.headers&&e.headers.Accept==="application/json"?t.json():t)}};})();
+(()=>{var n=class{constructor(){}async request(e){return fetch(e.url,e).then(t=>e.headers&&e.headers.Accept==="application/json"?t.json():t)}};})();
