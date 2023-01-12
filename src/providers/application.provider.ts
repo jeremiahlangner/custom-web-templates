677b04d0d65e8a9989171c1b45b05cdd24852b8c
Jeremiah Langner <jeremiah@jeremiahlangner.me>
2023-01-11 19:00:43 -0700

    fix data path function

--- src/providers/application.provider.ts
+++ src/providers/application.provider.ts
@@ -1,21 +1,28 @@
 export abstract class Application {
   static _app: any = {};
   static _data: any = {};
+  static _views;
+  static _components;
+  static _rules;
 
   get app() {
-    return this._app;
+    return Application._app;
   }
   set app(app: any) {
     Application._app = app;
-    window["vcdcApp"] = app;
   }
 
-  getByPath(obj: any, path: string) { // eslint-disable-line
-    const keys = path.split('.');
-    for (const key of keys) {
-      if (!obj[key]) return undefined;
-      obj = obj[key];
-    }
-    return obj;
+  getByPath(path: string) { // eslint-disable-line
+    const parts = path.split('.');
+    const getData = (obj: any) => { // eslint-disable-line
+      for (let i = 0; i < parts.length; i++) {
+        if (parts[i] === '$') continue;
+        if (obj[parts[i]] && i === parts.length - 1)
+          return obj[parts[i]];
+        obj = obj[parts[i]];
+      }
+    };
+    if (path === '$') return Application._data;
+    return getData(Application._data);
   }
 }
